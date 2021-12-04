9721230394c1c5175e16dbef168d535eff2f3637
smarthug <kirklayer@gmail.com>
2021-12-04 20:28:49 +0900

    touch?

--- src/App.js
+++ src/App.js
@@ -4,23 +4,22 @@ import { useEffect } from "react";
 
 function App() {
   useEffect(() => {
-    
     // offline 작동하게 만들기 ..
-
-
     // 대충 가는 코드
     // window.open("https://docs.google.com/spreadsheets/d/1Xk7d0O3o6inZ4JbAbQuwviIUJNSAfwfRCusjoiIGt6w/edit#gid=566299657")
     // window.location.replace("https://docs.google.com/spreadsheets/d/1Xk7d0O3o6inZ4JbAbQuwviIUJNSAfwfRCusjoiIGt6w/edit#gid=566299657")
-    
     // window.location.replace("https://docs.google.com/spreadsheets/d/1Xk7d0O3o6inZ4JbAbQuwviIUJNSAfwfRCusjoiIGt6w/edit?usp=sharing")
-
-
-    document.getElementById("thelink").click();
+    // document.getElementById("thelink").click();
   }, []);
+
+  function test() {
+    document.getElementById("thelink").click();
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={test}>TEST</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
