bd47d0b59df9461ddf90b7be24b034a0321bee70
smarthug <kirklayer@gmail.com>
2021-12-04 17:46:52 +0900

    init & go to done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    
+    // offline 작동하게 만들기 ..
+
+
+    // 대충 가는 코드
+    // window.open("https://docs.google.com/spreadsheets/d/1Xk7d0O3o6inZ4JbAbQuwviIUJNSAfwfRCusjoiIGt6w/edit#gid=566299657")
+    window.location.replace("https://docs.google.com/spreadsheets/d/1Xk7d0O3o6inZ4JbAbQuwviIUJNSAfwfRCusjoiIGt6w/edit#gid=566299657")
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
